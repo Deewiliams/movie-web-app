f745c8402654059930c02d41f4546fb3ebb28de5
Desire <desireirankundawilliams@gmail.com>
2022-07-01 15:23:24 +0200

    able to search for movies

--- src/Pages/PopularMovies.js
+++ src/Pages/PopularMovies.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Button, Container, Form, InputGroup } from "react-bootstrap";
 import HeaderTitle from "../Components/HeaderTitle";
+
+
 import { Link } from "react-router-dom";
 import Loading from "../Components/Loading";
-import Search from "../Components/Search";
 
 const PopularMovies = () => {
     const [PopularMovies, setPopularMovie] = useState([]);
+    const [query, setQuery] = useState("")
     const [loading, setLoading] = useState(false);
 
     const fetchPopularMovies = async () => {
@@ -28,34 +31,58 @@ const PopularMovies = () => {
     }, []);
     return (
         <>
-            {loading ? (<Loading />) : (<>
-                <br />
-                <HeaderTitle title='Most popular movies' />
-                <Search />
-                <div className="row">
-                    {PopularMovies.map((movie) => (
-                        <div className="col-sm-3" key={movie.id}>
-                            <br />
-                            <Link to={`/detail/${movie.id}`}>
-                            <div className="card">
-                                <img
-                                    className="rounded"
-                                    data-testid="poster_image"
-                                    src={`http://image.tmdb.org/t/p/w400${movie.poster_path}`}
-                                    alt={movie.title}
-                                />
-                            </div>
-                            </Link>
-                            <h3 className="card-title" data-testid="title">
-                                {movie.title}
-                            </h3>
-                        </div>
-                    ))}
-                </div>
-            </>
-            )}
+            {loading ? (<Loading />) : (
+                <>
+                    <br />
+                    <HeaderTitle title='Most popular movies' />
+                    <Container>
+                        <InputGroup className="mb-3">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                                onChange={event => setQuery(event.target.value)}
+                            />
+                        </InputGroup>
+                    </Container>
+                    <div className="row">
+                        {
+                            PopularMovies.filter(movie => {
+                                if (query === '') {
+                                    return movie;
+                                } else if (movie.title.toLowerCase().includes(query.toLowerCase())) {
+                                    return movie;
+                                }
+                            }).map((movie) => (
+                                <div className="col-sm-3" key={movie.id}>
+                                    <br />
+                                    <Link to={`/detail/${movie.id}`}>
+                                        <div className="card">
+                                            <img
+                                                className="rounded"
+                                                data-testid="poster_image"
+                                                src={`http://image.tmdb.org/t/p/w400${movie.poster_path}`}
+                                                alt={movie.title}
+                                            />
+                                        </div>
+                                    </Link>
+                                    <h3 className="card-title" data-testid="title">
+                                        {movie.title}
+                                    </h3>
+                                </div>
+                            ))}
+                    </div>
+                </>
+            )
+
+            }
+
         </>
     )
 }
 
+
+
 export default PopularMovies
+
